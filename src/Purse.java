2290de2bb3ae011b07d4e589917e5b5b64aaf5ce
Landon <N/A>
2024-09-09 13:37:45 -0500

    added the "Add" method that says if the denomination is in the purse already, update the count, if else, add it to the count

--- src/Purse.java
+++ src/Purse.java
@@ -1,2 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Purse {
+    private Map<Denomination, Integer> cash;
+
+    public Purse() {
+        cash = new HashMap<>();
+
+    }
+
+    public void add(Denomination type, int num) {
+        if (cash.containsKey(type)) {
+            cash.put(type, cash.get(type) + num);
+        }else {
+            cash.put(type, num);
+        }
+    }
+
+
+
+
+
+
+
 }
+
