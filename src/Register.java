6c1876c3f52e9ac21326695385d0205841a7aaa5
Landon <N/A>
2024-09-09 14:13:34 -0500

    Register Class

---
+++ src/Register.java
@@ -0,0 +1,27 @@
+public class Register {
+
+private static final Denomination[] denominations = {
+        new Denomination("Hundred Note", 100.0, "bill", "hundred.png"),
+        new Denomination("Fifty Note", 50.0, "bill", "fifty.png"),
+        new Denomination("Twenty Note", 20.0, "bill", "twenty.png"),
+        new Denomination("Ten Note", 10.0, "bill", "ten.png"),
+        new Denomination("Five Note", 5.0, "bill", "five.png"),
+        new Denomination("One Note", 1.0, "bill", "one.png"),
+        new Denomination("Quarter", 0.25, "coin", "quarter.png"),
+        new Denomination("Dime", 0.10, "coin", "dime.png"),
+        new Denomination("Nickle", 0.05, "coin", "nickle.png"),
+        new Denomination("Penny", 0.01, "coin", "penny.png")
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+}
